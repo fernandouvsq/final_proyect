45720eb3e06341708d4430f440a56a9a036e7951
LsanDterMOR <nicolas.vignard@epitech.eu>
2021-10-27 17:04:06 +0200

    [ADD] components + get user methods

---
+++ web_interfaces/bootstraptwo/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+//import CurrentWeather from './components/CurrentWeather.vue';
+import User from './components/User.vue';
+//import CharManager from './components/CharManager.vue';
+//import ClockManager from './components/ClockManager.vue';
+//import WorkingTime from './components/WorkingTime.vue';
+//import WorkingTimes from './components/WorkingTimes.vue';
+
+</script>
+
+<template>
+  <User />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ web_interfaces/bootstraptwo/src/components/User.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>test</div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+        path: "http://localhost:4000/api/user",
+        user: { email: "test@gmail.com", username: "test" }
+    };
+  },
+  methods: {
+    getUser: function () {
+      axios.get(this.path + "?email=" + this.user.email + "&username=" + this.user.username);
+    },
+    createUser: function (email, username) {
+        axios.post(this.path, this.user);
+    },
+    updateUser: function (email, username) {},
+    deleteUser: function (email, username) {},
+  },
+};
+</script>
+
+<style scoped>
+div #select {
+  color: #6a66d8;
+  font-size: 40px;
+}
+</style>
