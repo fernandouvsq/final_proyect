99bce51425dff4aad5899c32a1fb2ad4ee09f074
Thomas-Fayet <t.fayet@hotmail.fr>
2021-10-31 21:33:37 +0100

    update 'create' for WorkingTime

--- web_interfaces/src/components/WorkingTime.vue
+++ web_interfaces/src/components/WorkingTime.vue
@@ -4,9 +4,9 @@
       <v-container>
         <v-row>
           <v-col cols="3" sm="6" md="3">
-            <input type="date" v-model="dateStart" placeholder="Date de début">
-            <input type="date" v-model="dateEnd" placeholder="Date de fin">
-            <v-btn v-on:click="createWorkingTime" color="accent" elevation="2">Créer</v-btn>
+            <input type="datetime-local" v-model="dateStart" placeholder="Date de début">
+            <input type="datetime-local" v-model="dateEnd" placeholder="Date de fin">
+            <v-btn v-on:click="createWorkingTime(1)" color="accent" elevation="2">Créer</v-btn>
           </v-col>
         </v-row>
       </v-container>
@@ -18,19 +18,32 @@
 </template>
 
 <script>
+import axios from 'axios'
 
 export default {
   name: 'WorkingTime',
   data () {
     return {
       path: 'http://localhost:4000/api/workingtimes',
-      dateStart: null,
-      dateEnd: null,
-      id: 1
+      dateStart: '',
+      dateEnd: '',
+      userId: 1 // current_user in future
     }
   },
   methods: {
-    createWorkingTime () {
+    createWorkingTime (userId) {
+      axios
+        .post(this.path + '/' + userId, {
+          workingtime: {
+            start: this.dateStart,
+            end: this.dateEnd
+          }
+        })
+        .then((response) => {
+          console.log(response.data)
+          location.reload()
+        })
+        .catch(err => console.log(err.message))
     },
     updateWorkingTime () {
     },
